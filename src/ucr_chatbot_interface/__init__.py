7a06fff6481e313c9dd898c79de6ee265e08b68f
AgentWombat <jtzingale@gmail.com>
2025-10-16 17:51:44 -0700

    initial commit

---
+++ src/ucr_chatbot_interface/__init__.py
@@ -0,0 +1,12 @@
+"""A chainlit-powered web server for interacting with an AI Tutor."""
+
+from pathlib import Path
+
+from chainlit.utils import mount_chainlit
+import fastapi
+
+def create_app() -> fastapi.FastAPI:
+    app = fastapi.FastAPI()
+
+    mount_chainlit(app=app, target=str(Path(__file__).parent / "cl_app.py"), path="/")
+    return app
---
+++ src/ucr_chatbot_interface/__main__.py
@@ -0,0 +1,14 @@
+"""A chainlit-powered web server for interacting with an AI Tutor."""
+
+from argparse import ArgumentParser
+import uvicorn
+
+from . import create_app
+
+parser = ArgumentParser(description=__doc__)
+parser.add_argument("--host", type=str, default="127.0.0.1", help="the IP address for the application")
+parser.add_argument("--port", type=str, default="8000", help="the port on which the application")
+args = parser.parse_args()
+
+app = create_app()
+uvicorn.run(app, host=args.host, port=args.port)
